87021e8193f87d9918b5b3cc15eab8f28a8c1447
Hlib Pylypets <pilipets.gleb@gmail.com>
2020-10-13 02:15:18 +0300

    Upgraded console wrapper for the script

--- CodeFormatter(Java)/CodeFormatter(Java)/src/FormatterUI.py
+++ CodeFormatter(Java)/CodeFormatter(Java)/src/FormatterUI.py
@@ -3,6 +3,12 @@ import sys
 import os
 
 class FormatterUI:
+    help_text = ("------------------This is simple Python formatter for Java code---------------------\n"
+                  "Available execution templates are:\n"
+                  "1) name, action{--help, -h};\n"
+                  "2) name, action{--format, -f, --verify, -v}, template={path|default}, option{-(p|d|f)}, path.\n"
+                  "-------------------------------------------------------------------------------------")
+
     @staticmethod
     def report_error(x):
         print("Error: {}, use --help for more details!".format(x))
@@ -10,7 +16,6 @@ class FormatterUI:
 
     @staticmethod
     def prepare_formatting_files(option, path):
-        option = sys.argv[3]
         res = []
         if option == '-p':
             if not os.path.isdir(path):
@@ -38,53 +43,71 @@ class FormatterUI:
                 report_error("given path=%s isn't a file" % path)
             else:
                 return [path]
-        else:
-            report_error("working option '%s' path isn't supported" % option)
 
     @staticmethod
-    def process_files(action, files):
+    def process_files(action, files, in_path):
         formatter = CodeFormatter()
 
+        format_flag = False
         if action in ('--format', '-f'):
             func = formatter.format
+            format_option = True
         elif action in ('--verify', '-v'):
-            func = formatter.format
-        else:
-            report_error("action %s isn't supported" % sys.argv[2])
+            report_error("{} isn't supported".format(action))
 
+        success_cnt, errors_cnt = 0, 0
         for file in files:
-            print("Processing file=%s" % file)
             with open(file, "r") as f: javacode = f.read()
 
             tokens = jl.tokenizer.tokenize(javacode, ignore_errors= True)
             try:
-                output = formatter.format(tokens)
+                output = func(tokens)
             except BaseException as ex:
-                print("Exception received: %s" %ex)
-            
-            # TODO: add output to the respective directory
-            print(output)
+                print("Exception received when formatting file={}, ex={}".format(file, ex))
+                errors_cnt += 1
+            else:
+                success_cnt += 1
+
+            result_file = ('formatted_' if format_flag else 'verified_') + file
+            with open(result_file, "r") as f: f.write(output)
+
+        print(('Processed %d files successfully, %f files with errors' % success_cnt, errors_cnt))
 
     @staticmethod
     def handle_parameters():
-        if len(sys.argv) == 2:
+        params = set(sys.argv)
+
+        if len(sys.argv) != len(params):
+            report_error('incorrect parameters amount of the script')
+
+        if '-h' in params or '--help' in params:
             # name, action{--help, -h}
-            if not sys.argv[1] in ('--help', '-h'):
+            if len(params) != 2:
                 report_error("option '%s' isn't supported by the script" % sys.argv[1])
 
-            print("------------------This is simple Python formatter for Java code---------------------\n"
-                  "Available execution templates are:\n"
-                  "1) name, action{--help, -h};\n"
-                  "2) name, action{--format, -f, --verify, -v}, template={path|default}, option{-(p|d|f)}, path.\n"
-                  "-------------------------------------------------------------------------------------")
+            print(help_text)
 
-        elif len(sys.argv) == 5:
-            # name, action{--format, -f, --verify, -v}, template={path|default}, option{-(p|d|f)}, path
-            config_path = sys.argv[2].split('=')[1]
-            if config_path != 'default': report_error("config path isn't supported")
-
-            files = prepare_formatting_files(sys.argv[3], path)
-
-            process_files(sys.argv[3], files)
         else:
-            report_error("incorrect amount of script arguments - %s" % len(sys.argv))
+            # name, action{--format, -f, --verify, -v}, {--config, -c}=path, option{-(p|d|f)}, in_path
+            if len(params) > 5:
+                report_error("incorrect amount of the script arguments, given - %s" % len(params))
+
+            action = [c for c in ('-f', '--format', '--verify', '-v') if c in params]
+            if len(option) > 1:
+                report_error("incorrect usage of flags %s" % action)
+            else:
+                action = action[0] if len(action) else '-f'
+
+            if '--config' in params or '-c' in params:
+                report_error("config path isn't supported")
+
+            option = [c for c in ('-p', '-d', '-f') if c in params]
+            if len(option) > 1 or len(option) == 0:
+                report_error("incorrect usage of flags %s" % option)
+            else:
+                option = option[0]
+
+            in_path = sys.argv[-1]
+
+            files = prepare_formatting_files(option, in_path)
+            process_files(action, files, in_path)
